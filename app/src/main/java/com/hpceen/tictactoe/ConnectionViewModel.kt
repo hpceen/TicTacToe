e89b0f0859a55d0ea9e533a9c6ff6e6a24952119
HPCeen <hpceen@mail.ru>
2024-04-18 23:11:39 +0400

    ViewModel для Connection удалена.

--- app/src/main/java/com/hpceen/tictactoe/ConnectionViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.hpceen.tictactoe
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class ConnectionViewModel : ViewModel() {
-    var isConnected : MutableLiveData<Boolean> = MutableLiveData(false)
-}
